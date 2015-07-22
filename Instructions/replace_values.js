e9fd8b3983207614320259923243b4c1302df78e
yurippe <yurippenet@gmail.com>
2015-07-22 15:03:07 +0200

    Instruction files to make templates work in joomla

---
+++ Instructions/replace_values.js
@@ -0,0 +1,6 @@
+{
+	
+   "first-name": function(el) {el.innerHTML = window.prompt("First Name");},
+   "last-name": function(el) {el.innerHTML = window.prompt("Last Name");}
+    
+}
---
+++ Instructions/tinymce_fix.php
@@ -0,0 +1,8 @@
+				if (is_file(JPATH_ROOT . "/media/editors/tinymce/templates/template_list.js"))
+				{
+					$scripts_replace = file_get_contents(JPATH_ROOT . "/media/editors/tinymce/templates/replace_values.js");
+					$template_replacement_values = 'template_replace_values: '.$scripts_replace.',';
+					$templates .= '], '.$template_replacement_values;
+				}
+	
+				else{$templates .= '], ';}
